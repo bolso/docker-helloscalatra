eb893da64b75dc82a07ed7496f5de28695976cdc
root <root@agent1.acme.com>
2015-12-03 15:03:03 +0000

    added manifest

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+include 'docker'
+
+class docker-helloscalatra {
+
+	package { 'docker':
+
+		ensure => present,
+	}
+
+	docker::image { 'jads/hello-scalatra-jenkins':
+		image_tag => 'latest'
+	}
+
+	docker::run { 'hohoho':
+		image => 'jads/hello-scalatra-jenkins',
+}
+
