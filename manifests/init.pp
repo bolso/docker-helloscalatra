0382a63d2f6e0a8df79f36dd606e58bcc56604c3
Administrator <admin@example.com>
2015-12-04 10:40:11 +0000

    Kat's version

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,17 @@
 class docker-helloscalatra {
-
-	include 'docker'
-
-	docker::image { 'jads/hello-scalatra-jenkins':
-		image_tag => 'latest',
-	}
-
-	docker::run { 'hohoho':
-		image => 'jads/hello-scalatra-jenkins',
-		ports => ['8080:8080'],
-		pull_on_start => true,
-	}
+    include 'docker'
+    
+    docker::image { 'kizzie/hello-scalatra':
+      ensure => 'latest',
+    }
+    
+    exec { 'dockernuke':
+      command => '/usr/bin/docker rm -f $(docker ps -a -q)',
+    }
+    
+    docker::run { 'hello-scalatra':
+      image   => 'kizzie/hello-scalatra',
+      ports   => ['8080:8080'],
+      pull_on_start   => true,
+    }
 }
